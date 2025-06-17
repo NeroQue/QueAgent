1308fae401c45783522849abee3bd9dbddcb36d4
NeroQue <dominikbratek11@gmail.com>
2025-06-18 00:05:28 +0200

    Add new function schemas for file operations: `get_file_content`, `run_python_file`, and `write_file` in `schemas.py`

--- schemas.py
+++ schemas.py
@@ -17,9 +17,58 @@ def get_schemas():
         ),
     )
 
+    schema_get_file_content = types.FunctionDeclaration(
+        name="get_file_content",
+        description="Returns the content of the specified file.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "file_path": types.Schema(
+                    type=types.Type.STRING,
+                    description="The path to the file to get the content of, relative to the working directory.",
+                ),
+            },
+        ),
+    )
+
+    schema_run_python_file = types.FunctionDeclaration(
+        name="run_python_file",
+        description="Runs the specified Python file.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "file_path": types.Schema(
+                    type=types.Type.STRING,
+                    description="The path to the Python file to run, relative to the working directory.",
+                ),
+            },
+        ),
+    )
+
+    schema_write_file = types.FunctionDeclaration(
+        name="write_file",
+        description="Writes the specified content to the specified file.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write to, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+            },
+        ),
+    )
+
     # All available function declarations should be added to this list
     function_declarations = [
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 
     return types.Tool(function_declarations=function_declarations)
